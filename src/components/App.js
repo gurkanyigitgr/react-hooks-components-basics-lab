f708049297998ede3f56ada8c2ada87a8557b5da
Gürkan <gurkanyigitgr@gmail.com>
2023-06-06 16:31:50 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -15,14 +15,20 @@ function Home() {
     </div>
   );
 }
-
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Home />
+      <About />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
     </div>
